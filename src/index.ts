16d6ad27293deb3638b4165cdf0247cc68804173
Josiah Sayers <josiah.sayers15@gmail.com>
2022-10-17 18:43:04 -0400

    add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { app } from "./app";
-import { environment } from "./utils/environment";
+import { app } from './app';
+import { environment } from './utils/environment';
 
 app.listen(environment.port, () => {
   console.log(`App is listening on port ${environment.port}`);
