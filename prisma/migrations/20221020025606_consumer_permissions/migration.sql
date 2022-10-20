2b26211d25fa1f149eab7f5cfd75186346591980
Josiah Sayers <josiah.sayers15@gmail.com>
2022-10-19 22:58:40 -0400

    update migration for this pr

--- prisma/migrations/20221020025606_consumer_permissions/migration.sql
+++ prisma/migrations/20221020025606_consumer_permissions/migration.sql
@@ -4,19 +4,33 @@
   - A unique constraint covering the columns `[key]` on the table `Consumer` will be added. If there are existing duplicate values, this will fail.
 
 */
+-- CreateEnum
+CREATE TYPE "ConsumerStatus" AS ENUM ('valid', 'revoked');
+
 -- AlterTable
 ALTER TABLE "Consumer" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "status" "ConsumerStatus" NOT NULL DEFAULT 'valid',
 ADD COLUMN     "updatedAt" TIMESTAMP(3);
 
 -- CreateTable
 CREATE TABLE "ConsumerPermissions" (
     "id" SERIAL NOT NULL,
-    "getIngredient" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
+    "getIngredient" BOOLEAN NOT NULL DEFAULT false,
     "consumerId" INTEGER NOT NULL,
 
     CONSTRAINT "ConsumerPermissions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ConsumerNotes" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "note" TEXT NOT NULL,
+    "consumerId" INTEGER NOT NULL,
+
+    CONSTRAINT "ConsumerNotes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -27,3 +41,6 @@ CREATE UNIQUE INDEX "Consumer_key_key" ON "Consumer"("key");
 
 -- AddForeignKey
 ALTER TABLE "ConsumerPermissions" ADD CONSTRAINT "ConsumerPermissions_consumerId_fkey" FOREIGN KEY ("consumerId") REFERENCES "Consumer"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ConsumerNotes" ADD CONSTRAINT "ConsumerNotes_consumerId_fkey" FOREIGN KEY ("consumerId") REFERENCES "Consumer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
